0834103b1a232d244d0ae3800f46d2e8e7ca2838
River Kelly <75388756+RK-MSU@users.noreply.github.com>
2021-10-12 15:51:34 -0600

    Lab-5 - Fix #6

--- Labs/Lab-05/quote.js
+++ Labs/Lab-05/quote.js
@@ -1,4 +1,11 @@
+/*
+ * Lab 05 - Quote web API (XMLHttpRequest)
+ * 
+ */
+
+
 window.addEventListener("DOMContentLoaded", function () {
+
    document.querySelector("#fetchQuotesBtn").addEventListener("click", function () {
 
       // Get values from drop-downs
@@ -6,22 +13,49 @@ window.addEventListener("DOMContentLoaded", function () {
       const selectedTopic = topicDropdown.options[topicDropdown.selectedIndex].value;
       const countDropdown = document.querySelector("#countSelection");
       const selectedCount = countDropdown.options[countDropdown.selectedIndex].value;
-   
+
       // Get and display quotes
-      fetchQuotes(selectedTopic, selectedCount);	   
+      fetchQuotes(selectedTopic, selectedCount);
+
    });
+
 });
 
+
+
+
+
+// https://wp.zybooks.com/quotes.php?topic=love&count=3
 function fetchQuotes(topic, count) {
-   // TODO: Modify to use XMLHttpRequest
 
-   let html = "<ol>";
-   for (let c = 1; c <= count; c++) {
-      html += `<li>Quote ${c} - Anonymous</li>`;
-   }
-   html += "</ol>";
-
-   document.querySelector("#quotes").innerHTML = html;
+   // Create XMLHttpRequest URL (topic and count in the query string)
+   let req_url = `https://wp.zybooks.com/quotes.php?topic=${topic}&count=${count}`;
+   let xhttp = new XMLHttpRequest();   // Create an XMLHttpRequest object
+   xhttp.responseType = 'json';        // Set the response type to 'json'
+   xhttp.addEventListener('load', responseReceivedHandler);
+   // Send a request
+   xhttp.open("GET", req_url, true); 
+   xhttp.send();
 }
 
 // TODO: Add responseReceivedHandler() here
+function responseReceivedHandler() {
+
+   let data = this.response;
+
+   let html = "";
+
+   if(data.hasOwnProperty('error')) {
+      html = data['error'];
+   } else {
+      data.forEach(function(value, index) {
+         let quote = value['quote'];
+         let author = value['source'];
+         html += `<li>${quote} - ${author}</li>`;
+      });
+      html = `<ol>${html}</ol>`;
+   }
+
+   document.querySelector("#quotes").innerHTML = html;
+
+}
